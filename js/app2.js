54bffe0577006f86710489b8f95839af4c183947
Robbie Constantin <robbieconstantin@gmail.com>
2014-11-02 16:08:09 -0500

    file for jquery animation for about.html page

---
+++ js/app2.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+      $('.nav-tabs').click(function(){
+            console.log('got clicked');
+            $('.animate').slideToggle(1);
+            $('.animate').slideToggle(2000);
+      }); //end michele click
+
+}); // end doc ready
+
+$(window).load(function(){
+      $('.about-us').slideDown(2000);
+      $('.animate').slideToggle(1);
+      $('.animate').slideToggle(2000);
+});
